0afe6f98a9c5e0fe9470fda5d44270cc671a9dca
rfujino731 <wbhp.litw.twba.lib@gmail.com>
2020-05-02 00:58:50 +0900

    [finish]5-5

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,12 @@
 Rails.application.routes.draw do
+  # get 'users/show'
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-root 'post_images#index'
+  root 'post_images#index'
 
-resources :post_images, only: [:new, :create, :index, :show, :destroy] do 
+  resources :post_images, only: [:new, :create, :index, :show, :destroy] do 
 	resource :favorites, only: [:create, :destroy]
 	resource :post_comments, only: [:create, :destroy]
 end
+	resources :users, only: [:show]
 end
